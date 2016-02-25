ccd57182b2f63b5bf3fee27138939688b3422888
Marek Marecki <marekjm@ozro.pw>
2016-02-25 21:50:23 +0100

    One more target

--- example.js
+++ example.js
@@ -1,4 +1,8 @@
 ('build/bin/vm/cpu', ['src/front/cpu.cpp',]) -> (name, dependencies)
     cxx 'foo',
-    cxx 'bar',
+    cxx 'bar'
+.
+
+('build/bin/vm/asm', ['src/front/asm.cpp',]) -> (name, dependencies)
+    cxx 'build/bin/vm/asm'
 .
