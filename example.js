8d5101961e2b5335ec7abd6b181351a351fe5f7e
Marek Marecki <marekjm@ozro.pw>
2017-05-03 16:58:23 +0200

    Remove old examples

--- example.js
+++ example.js
@@ -1,28 +1,3 @@
-/*
-cxxflags = '-std=c++11 -Wall -Wextra -Wzero-as-null-pointer-constant -Wuseless-cast -Wconversion -Winline -pedantic -Wfatal-errors -g -I./include'.
-cxxoptimizationflags = ''.
-coptimizationflags = ''.
-dynamic_syms = '-Wl,--dynamic-list-cpp-typeinfo'.
-cxx = 'g++'.
-
-
-compile(output, compilation_members)
-    cxx '-o' output compilation_members
-.
-
-
-do ('build/bin/vm/cpu', ['src/front/cpu.cpp', 'build/cpu.o'],) -> (name, dependencies) ->
-    compile(name, dependencies)
-.
-
-do ('build/bin/vm/asm', ['src/front/asm.cpp',],) -> (name, dependencies) ->
-    cxx 'build/bin/vm/asm'
-.
-
-do ('build/bin/dummy',) -> (name) ->
-.
-*/
-
 let cxx = 'g++' .
 let cxxoptions = [
     '-Wall',
