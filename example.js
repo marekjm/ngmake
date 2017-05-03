8d58ddd5ca06d11a18e2de01cf24c442efede0a6
Marek Marecki <marekjm@ozro.pw>
2017-05-03 18:56:40 +0200

    Remove unneded code

--- example.js
+++ example.js
@@ -12,24 +12,6 @@ macro echo ( ...all ) ->
     'echo' ...all
 .
 
-/*
-do ('build/bin/vm/cpu', ['src/front/cpu.cpp', 'build/cpu.o',]) -> (name, deps) ->
-    'g++' '-o' name ...deps ,
-    echo ( ...deps, name )
-.
-
-let test = 'test' .
-do (test, []) -> (name, deps) ->
-    'g++' name ,
-    echo ( 'Hello World!' ) ,
-    echo ( ...deps )
-.
-
-do ('dafuq', ['foo', 'bar', 'bax', 'bay', 'baz',]) -> (name, deps) ->
-    echo ( name, ...deps )
-.
-*/
-
 macro nope () ->
     'Hello World!'
 .
@@ -52,11 +34,6 @@ do ('heh', ['0', '1', '2', '3', '4']) -> (name, deps) ->
     echo( ...reverse( ...deps, nope() ) )
 .
 
-/*
-macro compile ( output, ...deps ) ->
-    cxx ...cxxoptions '-o' output ...deps
-.
-*/
 macro compile ( name, ...all ) ->
     cxx ...cxxoptions '-o' name ...all ,
     'touch' name
