5c4dc054b7acd2da8670c5695dddf43a72c92cae
Marek Marecki <marekjm@ozro.pw>
2017-05-04 14:42:30 +0200

    Add "AUTHOR" and "COPYRIGHT" sections

--- ngmake.py
+++ ngmake.py
@@ -231,6 +231,27 @@ CONCEPTS
             do ('build/bin/foo', [ 'src/foo.cpp' ])
 
 
+AUTHOR
+
+    Ngmake is written and maintained by Marek Marecki.
+
+
+COPYRIGHT
+
+    Copyright (c) 2016-2017 Mareck Marecki
+
+    Ngmake is free software; you can redistribute it and/or modify it under the terms of
+    the GNU General Public License as published by the Free Software Foundation;
+    either version 3 of the License, or (at your option) any later version.
+
+    Ngmake is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+    without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with this program.
+    If not, see http://www.gnu.org/licenses/.
+
+
 SEE ALSO
 
     make(1)
