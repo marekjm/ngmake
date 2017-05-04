b083a5d1c51f730b59f7a0ea4a7f111110dc088e
Marek Marecki <marekjm@ozro.pw>
2017-05-04 14:47:21 +0200

    Add one more example

---
+++ examples/lists.js
@@ -0,0 +1,25 @@
+/* Gather parameters into a list. */
+macro gather ( ...all ) -> all .
+
+/* Reverse parameters. */
+macro reverse ( first, ...rest ) ->
+    gather( ...reverse( ...rest ), first )
+; reverse ( only ) ->
+    gather( only )
+.
+
+/* Return head or tail of a list. */
+macro head ( first, ...rest ) -> first .
+macro tail ( first, ...rest ) -> rest .
+
+/* Echo all parameters. */
+macro echo ( ...all ) -> 'echo' ...all .
+
+/* A test target to check output generation. */
+do ('test', [ 'Hello', 'beautiful', 'World' ]) -> (name, deps) ->
+    echo( name ),
+    echo( ...deps ),
+    echo( ...reverse( ...deps ) ),
+    echo( head( ...deps ) ),
+    echo( ...tail( ...deps ) )
+.
